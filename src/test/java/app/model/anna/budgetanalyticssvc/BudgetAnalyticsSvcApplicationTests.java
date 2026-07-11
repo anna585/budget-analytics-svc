40fc8dcbb4debe0142bdbaa8cb52321231b18c16
AnnaPetrova <anna.angova@gmail.com>
2026-07-11 22:18:52 +0300

    Initial commit

---
+++ src/test/java/app/model/anna/budgetanalyticssvc/BudgetAnalyticsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package app.model.anna.budgetanalyticssvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAnalyticsSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
