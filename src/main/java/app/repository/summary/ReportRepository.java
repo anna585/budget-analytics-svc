1dd535d61ff896d0e9668a1dea4cfac417e4517c
AnnaPetrova <anna.angova@gmail.com>
2026-07-16 22:41:05 +0300

    feat: create repository Report

--- src/main/java/app/repository/summary/ReportRepository.java
+++ src/main/java/app/repository/summary/ReportRepository.java
@@ -1,11 +1,11 @@
-package app.repository;
+package app.repository.summary;
 
-import app.model.entities.Summary;
+import app.model.entities.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface SummaryRepository extends JpaRepository<Summary, UUID> {
+public interface ReportRepository extends JpaRepository<Report, UUID> {
 }
