a94211284d5ab75ce9e2a23d29ed3470a7e7ccad
AnnaPetrova <anna.angova@gmail.com>
2026-07-17 12:16:25 +0300

    feat: create generateSummary POST method

--- src/main/java/app/web/SummaryController.java
+++ src/main/java/app/web/SummaryController.java
@@ -1,7 +1,25 @@
 package app.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import app.service.ReportService;
+import app.web.dto.SummaryResponse;
+import app.web.dto.TransactionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
+@RequestMapping("/api/v1/analytics/summary")
+@RequiredArgsConstructor
 public class SummaryController {
+
+    private final ReportService reportService;
+
+    @PostMapping
+    SummaryResponse generateSummary(@RequestBody List<TransactionDto> transactions){
+
+       return reportService.getSummary(transactions);
+
+    }
 }
