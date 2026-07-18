2cd5183c73f0cc425d87f332770d6d37f4aba216
AnnaPetrova <anna.angova@gmail.com>
2026-07-18 10:55:45 +0300

    feat: add id field

--- src/main/java/app/web/dto/ReportResponse.java
+++ src/main/java/app/web/dto/ReportResponse.java
@@ -6,11 +6,13 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @Builder
 public class ReportResponse {
 
+    private UUID id;
     private BigDecimal income;
     private BigDecimal expenses;
     private BigDecimal balance;
