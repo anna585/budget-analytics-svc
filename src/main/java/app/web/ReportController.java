ca9dba63259215dd73909b8cfb13a06cfb93a5f1
AnnaPetrova <anna.angova@gmail.com>
2026-07-18 10:53:56 +0300

    feat: create GET history and DELETE report endpoints

--- src/main/java/app/web/ReportController.java
+++ src/main/java/app/web/ReportController.java
@@ -4,7 +4,11 @@ import app.service.ReportService;
 import app.web.dto.ReportRequest;
 import app.web.dto.ReportResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,23 @@ public class ReportController {
 
       return reportService.postReport(reportRequest);
     }
+
+    @GetMapping("/api/v1/analystics/report-history")
+    public ResponseEntity<List<ReportResponse>> getHistory(
+            @RequestParam String userId) {
+
+        List<ReportResponse> responseList = reportService.getReportHistory(userId);
+        return ResponseEntity.ok(responseList);
+    }
+
+    @DeleteMapping("/api/v1/analytics/report-history/{reportId}")
+    public ResponseEntity<Void> deleteReport(
+            @PathVariable String reportId){
+
+        reportService.deleteReport(reportId);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
 
 }
