d52975f5af43c0331747be0317276d88df0f6e1d
AnnaPetrova <anna.angova@gmail.com>
2026-07-18 10:56:32 +0300

    feat: create findByUserId and findReportById

--- src/main/java/app/repository/report/ReportRepository.java
+++ src/main/java/app/repository/report/ReportRepository.java
@@ -1,11 +1,16 @@
-package app.repository.summary;
+package app.repository.report;
 
 import app.model.entities.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, UUID> {
+
+    List<Report> findByUserId(UUID userId);
+
+    Report findReportById(UUID id);
 }
