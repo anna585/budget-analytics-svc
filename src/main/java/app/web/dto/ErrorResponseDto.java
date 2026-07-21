2938b0b6328998ce99d75b336e03546ce31823e7
AnnaPetrova <anna.angova@gmail.com>
2026-07-21 21:55:58 +0300

    feat: create ErrorResponseDto

---
+++ src/main/java/app/web/dto/ErrorResponseDto.java
@@ -0,0 +1,22 @@
+package app.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+
+}
