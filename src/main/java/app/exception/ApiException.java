06eb7a14a6674666a78f0db53954a1a899e2e076
AnnaPetrova <anna.angova@gmail.com>
2026-07-23 18:15:58 +0300

    feat: create ApiException extends RuntimeException

---
+++ src/main/java/app/exception/ApiException.java
@@ -0,0 +1,13 @@
+package app.exception;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class ApiException extends RuntimeException{
+
+    public ApiException(String message){
+
+        super(message);
+    }
+
+    public abstract HttpStatus getHttpStatus();
+}
